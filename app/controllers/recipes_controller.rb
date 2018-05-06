adbe49b94d47879dff345627ae6f1c2d6a598549
williamsjs <williamsjs1224@gmail.com>
2018-05-06 15:48:54 -0400

    modify recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,40 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :update, :destroy]
+
+  def index
+    @recipes = Recipe.all
+
+    render json: @recipes, status: :ok
+  end
+
+  def show
+    render json: @recipe, status: :ok
+  end
+
+  def create
+    @recipe = Recipe.create!(recipe_params)
+
+    render json: @recipe, statue: :created
+  end
+
+  def update
+    @recipe.update(recipe_params)
+    head :no_content
+  end
+
+  def destroy
+    @recipe.destroy
+    head :no_content
+  end
+
+  private
+
+  def recipe_params
+    params.permit(:name)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
 end
