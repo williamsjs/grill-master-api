0beeb64abae630d4ff055a03c3881c5b17e4da2a
williamsjs <williamsjs1224@gmail.com>
2018-05-06 18:51:16 -0400

    allow requests from localhost:8080

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,13 @@ module GrillMasterApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Access-Control-Allow-Origin
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:8080', /https*:\/\/.*?bloopist\.com/
+        resource '*', :headers => :any, :methods => :any
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
